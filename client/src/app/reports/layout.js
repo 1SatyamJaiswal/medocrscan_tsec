303d9f5f78d0b77d607dda46731514189fbbe84c
1SatyamJaiswal <satyamjaiswal9752@gmail.com>
2024-01-31 11:28:35 +0530

    made the components folder

---
+++ client/src/app/reports/layout.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Sidebar from "@/components/Sidebar";
+
+const Reports = () => {
+  return (
+    <>
+      <Sidebar />
+      <div>Reports</div>
+    </>
+  );
+};
+
+export default Reports;
